024a4f7ec8b9468e18e535d11356dd9f63e62fc7
Chai Phonbopit <phonbopit@gmail.com>
2018-05-01 17:20:36 +0700

    Create simple echo routes

---
+++ test/index.spec.js
@@ -0,0 +1,36 @@
+const Hapi = require('hapi')
+const test = require('tape')
+const echoRoutes = require('../lib')
+
+const server = Hapi.server({ port: 5555 })
+
+// Mock server
+server.route({
+	method: 'GET',
+	path: '/api/route1',
+	handler: (req, h) => 'route 1'
+})
+
+server.route({
+	method: 'GET',
+	path: '/api/route2',
+	handler: (req, h) => 'route 2'
+})
+
+server.route({
+	method: 'POST',
+	path: '/api/route2',
+	handler: (req, h) => 'success'
+})
+
+server.register(echoRoutes)
+
+const e = `GET\t/api/route1
+GET\t/api/route2
+POST\t/api/route2`
+
+test('Display all routes', t => {
+	t.plan(1)
+	t.equal(server.app.routes, e)
+})
+
